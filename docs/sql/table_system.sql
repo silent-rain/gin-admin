92ef6c63b297becda742c20ac7680179c9d60131
silent-rain <silent_rains@1163.com>
2023-01-12 00:00:06 +0800

    sql逻辑调整

--- docs/sql/table_system.sql
+++ docs/sql/table_system.sql
@@ -25,6 +25,24 @@ CREATE TABLE user (
     PRIMARY KEY (`id`)
 ) ENGINE = InnoDB DEFAULT CHARACTER SET = utf8mb4 COLLATE = utf8mb4_unicode_ci COMMENT '用户';
 
+-- user表触发器，更新其他表冗余字段
+/*
+ CREATE TRIGGER trigger_update_user
+ AFTER
+ UPDATE
+ ON `user` FOR EACH ROW BEGIN
+ 
+ IF NEW.nickname != OLD.nickname THEN 
+ -- 更新 api_token.nickname 字段
+ UPDATE
+ api_token
+ SET
+ nickname = NEW.nickname
+ WHERE
+ user_id = NEW.id;
+ END IF;
+ END;
+ */
 -- 用户位置 - 待定
 CREATE TABLE user_location (
     `id` INT AUTO_INCREMENT COMMENT '位置ID',
@@ -67,7 +85,7 @@ CREATE TABLE user_login_token (
 ) ENGINE = InnoDB DEFAULT CHARACTER SET = utf8mb4 COLLATE = utf8mb4_unicode_ci COMMENT '用户登录Token表-用于登录';
 
 /*
- 网络相关的表
+ 日志相关表
  */
 -- 网络请求日志表
 CREATE TABLE http_log (
@@ -88,31 +106,49 @@ CREATE TABLE http_log (
     PRIMARY KEY (`id`)
 ) ENGINE = InnoDB DEFAULT CHARACTER SET = utf8mb4 COLLATE = utf8mb4_unicode_ci COMMENT '网络请求日志';
 
-/*
- 授权相关的表
- */
--- 用户Token信息表
-CREATE TABLE user_token (
+-- 系统日志表
+CREATE TABLE system_log (
+    `id` INT AUTO_INCREMENT COMMENT '自增ID',
+    `user_id` INT NULL COMMENT '请求用户ID',
+    `trace_id` VARCHAR(32) NULL COMMENT '请求traceId',
+    `level` INT(10) NOT NULL COMMENT '日志级别',
+    `caller_line` VARCHAR(100) NOT NULL COMMENT '日志发生位置',
+    `error_code` VARCHAR(10) NULL COMMENT '业务错误码',
+    `error` VARCHAR(100) NOT NULL COMMENT '业务错信息',
+    `msg` VARCHAR(100) NULL COMMENT '日志消息',
+    `extend` VARCHAR(500) NOT NULL COMMENT '日志扩展信息/json',
+    `note` VARCHAR(255) NULL COMMENT '备注',
+    `created_at` DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT '创建时间',
+    PRIMARY KEY (`id`)
+) ENGINE = InnoDB DEFAULT CHARACTER SET = utf8mb4 COLLATE = utf8mb4_unicode_ci COMMENT '系统日志';
+
+/*API授权相关的表 */
+-- API_Token令牌表
+CREATE TABLE api_token (
     `id` INT AUTO_INCREMENT COMMENT '自增ID',
     `user_id` INT(20) NOT NULL COMMENT '用户ID',
-    `token` VARCHAR(50) NOT NULL COMMENT 'Token 信息',
+    `token` VARCHAR(50) NOT NULL COMMENT 'Token信息',
     `status` TINYINT(1) NOT NULL DEFAULT 1 COMMENT '是否启用,0:禁用,1:启用',
     `created_at` DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT '创建时间',
     `updated_at` DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT '更新时间',
     PRIMARY KEY (`id`),
-    UNIQUE KEY `user_token_uni_user_id` (`user_id`)
-) ENGINE = InnoDB DEFAULT CHARACTER SET = utf8mb4 COLLATE = utf8mb4_unicode_ci COMMENT '用户Token信息表-用于授权';
+    UNIQUE KEY `uni_api_token_user_id` (`user_id`),
+    CONSTRAINT `api_token_user_id` FOREIGN KEY (`user_id`) REFERENCES `user` (`id`) ON DELETE CASCADE ON UPDATE CASCADE
+) ENGINE = InnoDB DEFAULT CHARACTER SET = utf8mb4 COLLATE = utf8mb4_unicode_ci COMMENT 'API_Token令牌表';
 
--- 用户Token的API授权
-CREATE TABLE user_token_api_auth (
+-- API_Token_URI授权表,基于Token授权
+CREATE TABLE api_token_uri_auth (
     `id` INT AUTO_INCREMENT COMMENT '自增ID',
-    `user_token_id` INT(11) NOT NULL COMMENT '用户Token信息表ID',
+    `api_token_id` INT(11) NOT NULL COMMENT 'API-Token令牌ID',
     `uri` VARCHAR(200) NOT NULL COMMENT '请求地址路径',
     `expire` DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT '授权到期时间',
     `status` TINYINT(1) NOT NULL DEFAULT 1 COMMENT '是否启用,0:禁用,1:启用',
     `created_at` DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT '创建时间',
     `updated_at` DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT '更新时间',
     PRIMARY KEY (`id`),
-    UNIQUE KEY `user_token_api_auth_uni_user_token_id` (`user_token_id`, `uri`),
-    CONSTRAINT `user_token_api_auth_user_token_id` FOREIGN KEY (`user_token_id`) REFERENCES `user_token` (`id`) ON DELETE CASCADE
-) ENGINE = InnoDB DEFAULT CHARACTER SET = utf8mb4 COLLATE = utf8mb4_unicode_ci COMMENT '用户Token的API授权';
+    UNIQUE KEY `uni_api_token_uri_auth_api_token_id_uri` (`api_token_id`, `uri`),
+    CONSTRAINT `api_token_uri_auth_api_token_id` FOREIGN KEY (`api_token_id`) REFERENCES `api_token` (`id`) ON DELETE CASCADE ON UPDATE CASCADE
+) ENGINE = InnoDB DEFAULT CHARACTER SET = utf8mb4 COLLATE = utf8mb4_unicode_ci COMMENT 'API_Token_URI授权表';
+
+-- api_token_role API_Token角色关联表,基于角色授权
+-- api_uri_role API_URI角色关联表
