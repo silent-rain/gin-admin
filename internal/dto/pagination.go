b00378eb90bd7ba817506b85aa47adee4ce0faae
silent-rain <silent_rains@1163.com>
2023-01-31 00:17:22 +0800

    分页查询优化

--- internal/dto/pagination.go
+++ internal/dto/pagination.go
@@ -13,15 +13,21 @@ package dto
 // Pagination 分页查询条件
 type Pagination struct {
 	Page     int `json:"page" form:"page"`           // 页码，从1开始
-	PageSize int `json:"page_size" form:"page_size"` // 每页数
+	PageSize int `json:"page_size" form:"page_size"` // 每页数, 默认100
 }
 
 // 获取页数
-func (p Pagination) getPage() int {
+func (p *Pagination) getPage() int {
+	if p.Page == 0 {
+		p.Page = 1
+	}
 	return p.Page - 1
 }
 
 // Offset 偏移行数
-func (p Pagination) Offset() int {
+func (p *Pagination) Offset() int {
+	if p.PageSize == 0 {
+		p.PageSize = 100
+	}
 	return p.getPage() * p.PageSize
 }
