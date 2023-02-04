3e352482eecf939778948de3ffd1501d02dcc44c
silent-rain <silent_rains@1163.com>
2023-02-04 12:24:32 +0800

    orm返回字段时间优化

--- internal/model/system/http_log.go
+++ internal/model/system/http_log.go
@@ -10,24 +10,24 @@
  */
 package systemModel
 
-import "time"
+import "gin-admin/internal/pkg/utils"
 
 // HttpLog 网络请求日志
 type HttpLog struct {
-	ID         uint      `gorm:"column:id;primaryKey"`                   // 自增ID
-	UserId     uint      `gorm:"column:user_id"`                         // 请求用户ID
-	TraceId    string    `gorm:"column:trace_id"`                        // 请求traceId
-	StatusCode int       `gorm:"column:status_code"`                     // 请求状态码
-	Method     string    `gorm:"column:method"`                          // 请求方法
-	Path       string    `gorm:"column:path"`                            // 请求地址路径
-	Query      string    `gorm:"column:query"`                           // 请求参数
-	Body       string    `gorm:"column:body"`                            // 请求体/响应体
-	RemoteAddr string    `gorm:"column:remote_addr"`                     // 请求IP
-	UserAgent  string    `gorm:"column:user_agent"`                      // 用户代理
-	Cost       int64     `gorm:"column:cost"`                            // 耗时,纳秒
-	HttpType   string    `gorm:"column:htpp_type"`                       // 日志类型:req/rsp
-	Note       string    `gorm:"column:note"`                            // 备注
-	CreatedAt  time.Time `gorm:"column:created_at;autoCreateTime:milli"` // 创建时间
+	ID         uint             `gorm:"column:id;primaryKey"`                   // 自增ID
+	UserId     uint             `gorm:"column:user_id"`                         // 请求用户ID
+	TraceId    string           `gorm:"column:trace_id"`                        // 请求traceId
+	StatusCode int              `gorm:"column:status_code"`                     // 请求状态码
+	Method     string           `gorm:"column:method"`                          // 请求方法
+	Path       string           `gorm:"column:path"`                            // 请求地址路径
+	Query      string           `gorm:"column:query"`                           // 请求参数
+	Body       string           `gorm:"column:body"`                            // 请求体/响应体
+	RemoteAddr string           `gorm:"column:remote_addr"`                     // 请求IP
+	UserAgent  string           `gorm:"column:user_agent"`                      // 用户代理
+	Cost       int64            `gorm:"column:cost"`                            // 耗时,纳秒
+	HttpType   string           `gorm:"column:htpp_type"`                       // 日志类型:req/rsp
+	Note       string           `gorm:"column:note"`                            // 备注
+	CreatedAt  *utils.LocalTime `gorm:"column:created_at;autoCreateTime:milli"` // 创建时间
 }
 
 // TableName 表名重写
--- internal/model/system/user.go
+++ internal/model/system/user.go
@@ -11,28 +11,29 @@
 package systemModel
 
 import (
+	"gin-admin/internal/pkg/utils"
 	"time"
 )
 
 // User 用户表
 type User struct {
-	ID        uint      `json:"id" gorm:"column:id;primaryKey"`                           // 用户ID
-	Realname  string    `json:"realname" gorm:"column:realname"`                          // 真实姓名
-	Nickname  string    `json:"nickname" gorm:"column:nickname"`                          // 昵称
-	Gender    uint      `json:"gender" gorm:"column:gender"`                              // 性别: 0:保密,1:女,2:男
-	Age       uint8     `json:"age" gorm:"column:age"`                                    // 年龄
-	Birthday  string    `json:"birthday" gorm:"column:birthday"`                          // 出生日期
-	Avatar    string    `json:"avatar" gorm:"column:avatar"`                              // 用户头像URL
-	Phone     string    `json:"phone" gorm:"column:phone"`                                // 手机号码
-	Email     string    `json:"email" gorm:"column:email"`                                // 邮箱
-	Intro     string    `json:"intro" gorm:"column:intro"`                                // 介绍
-	Note      string    `json:"note" gorm:"column:note"`                                  // 备注
-	Password  string    `json:"password" gorm:"column:password;->:false;<-:create"`       // 密码, 仅创建（禁止从 db 读）
-	Sort      uint      `json:"sort" gorm:"column:sort"`                                  // 排序
-	Status    uint      `json:"status" gorm:"column:status"`                              // 是否启用,0:禁用,1:启用
-	CreatedAt time.Time `json:"created_at" gorm:"column:created_at;autoCreateTime:milli"` // 创建时间
-	UpdatedAt time.Time `json:"updated_at" gorm:"column:updated_at;autoUpdateTime:milli"` // 更新时间
-	Roles     []Role    `json:"roles" gorm:"many2many:sys_user_role_rel;"`                // 角色列表, Many To Many, 关联 role 表
+	ID        uint             `json:"id" gorm:"column:id;primaryKey"`                           // 用户ID
+	Realname  string           `json:"realname" gorm:"column:realname"`                          // 真实姓名
+	Nickname  string           `json:"nickname" gorm:"column:nickname"`                          // 昵称
+	Gender    uint             `json:"gender" gorm:"column:gender"`                              // 性别: 0:保密,1:女,2:男
+	Age       uint8            `json:"age" gorm:"column:age"`                                    // 年龄
+	Birthday  string           `json:"birthday" gorm:"column:birthday"`                          // 出生日期
+	Avatar    string           `json:"avatar" gorm:"column:avatar"`                              // 用户头像URL
+	Phone     string           `json:"phone" gorm:"column:phone"`                                // 手机号码
+	Email     string           `json:"email" gorm:"column:email"`                                // 邮箱
+	Intro     string           `json:"intro" gorm:"column:intro"`                                // 介绍
+	Note      string           `json:"note" gorm:"column:note"`                                  // 备注
+	Password  string           `json:"password" gorm:"column:password;->:false;<-:create"`       // 密码, 仅创建（禁止从 db 读）
+	Sort      uint             `json:"sort" gorm:"column:sort"`                                  // 排序
+	Status    uint             `json:"status" gorm:"column:status"`                              // 是否启用,0:禁用,1:启用
+	CreatedAt *utils.LocalTime `json:"created_at" gorm:"column:created_at;autoCreateTime:milli"` // 创建时间
+	UpdatedAt *utils.LocalTime `json:"updated_at" gorm:"column:updated_at;autoUpdateTime:milli"` // 更新时间
+	Roles     []Role           `json:"roles" gorm:"many2many:sys_user_role_rel;"`                // 角色列表, Many To Many, 关联 role 表
 }
 
 // TableName 表名重写
