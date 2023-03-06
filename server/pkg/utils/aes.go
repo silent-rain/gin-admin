da6088af30f46ae5442168dfcfb9cee8964cccbd
silent-rain <silent_rains@1163.com>
2023-03-06 22:05:38 +0800

    添加aes解密模块

---
+++ server/pkg/utils/aes.go
@@ -0,0 +1,48 @@
+/*Aes 加密*/
+package utils
+
+import (
+	"bytes"
+	"crypto/aes"
+	"crypto/cipher"
+)
+
+func PKCS5Padding(cipherText []byte, blockSize int) []byte {
+	padding := blockSize - len(cipherText)%blockSize
+	padText := bytes.Repeat([]byte{byte(padding)}, padding)
+	return append(cipherText, padText...)
+}
+
+func PKCS5UnPadding(origData []byte) []byte {
+	length := len(origData)
+	unPadding := int(origData[length-1])
+	return origData[:(length - unPadding)]
+}
+
+func AesEncrypt(origData, key []byte) ([]byte, error) {
+	block, err := aes.NewCipher(key)
+	if err != nil {
+		return nil, err
+	}
+
+	blockSize := block.BlockSize()
+	origData = PKCS5Padding(origData, blockSize)
+	blockMode := cipher.NewCBCEncrypter(block, key[:blockSize])
+	crypt := make([]byte, len(origData))
+	blockMode.CryptBlocks(crypt, origData)
+	return crypt, nil
+}
+
+func AesDecrypt(crypt, key []byte) ([]byte, error) {
+	block, err := aes.NewCipher(key)
+	if err != nil {
+		return nil, err
+	}
+
+	blockSize := block.BlockSize()
+	blockMode := cipher.NewCBCDecrypter(block, key[:blockSize])
+	origData := make([]byte, len(crypt))
+	blockMode.CryptBlocks(origData, crypt)
+	origData = PKCS5UnPadding(origData)
+	return origData, nil
+}
